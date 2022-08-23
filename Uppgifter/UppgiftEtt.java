7947dd3606be1b76d1043797bfc507efdcf02447
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2022-08-23 16:20:26 +0200

    change i too 1

--- Uppgifter/UppgiftEtt.java
+++ Uppgifter/UppgiftEtt.java
@@ -3,10 +3,10 @@ package Uppgifter;
 public class UppgiftEtt {
     public static void main(String[] args) {
 
-        int i = 0;
+        int i = 1;
         int remainder = 0;
 
-        for (i = 0; i <= 100; i++) {
+        for (i = 1; i <= 100; i++) {
             if (i == 42) {
                 System.out.println("Answer to the Ultimate Question of Life, the Universe, and Everything");
             } else if (i % 3 == remainder && i % 5 == remainder) {
