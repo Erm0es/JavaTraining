306e006f6f9d9c70396d24e10b5be04d50dbb5de
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2022-08-23 13:58:04 +0200

    first commit

---
+++ Uppgifter/UppgiftEtt.java
@@ -0,0 +1,23 @@
+package Uppgifter;
+
+public class UppgiftEtt {
+    public static void main(String[] args) {
+
+        int i = 0;
+        int remainder = 0;
+
+        for (i = 0; i <= 100; i++) {
+            if (i == 42) {
+                System.out.println("Answer to the Ultimate Question of Life, the Universe, and Everything");
+            } else if (i % 3 == remainder && i % 5 == remainder) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == remainder) {
+                System.out.println("Fizz");
+            } else if (i % 3 == remainder) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
