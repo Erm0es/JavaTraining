dd3e186234d218e2d86af32e3536e9c36f3a258c
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2022-08-29 09:33:09 +0200

    Work on filecrawler

--- Uppgifter/FileCrawler.java
+++ Uppgifter/FileCrawler.java
@@ -1,6 +1,8 @@
 package Uppgifter;
 
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.io.*;
 
 public class FileCrawler {
     public static void main (String [] args){
@@ -10,5 +12,23 @@ public class FileCrawler {
         String search = scanner.nextLine();
         System.out.println(search);
         scanner.close();
+
+        readFile("C:\\Users\\Emelie\\Desktop\\Skolan\\FileCrawler");
+    }
+
+    private static void readFile(String filePath){
+        File file = new File(filePath);
+
+        Scanner fileScanner = null;
+        try {
+            fileScanner = new Scanner(file);
+            while(fileScanner.hasNext()) {
+                System.out.println(fileScanner.nextLine());
+            }
+        } catch(FileNotFoundException e){
+            System.out.println("File not found.");
+        } finally {
+            fileScanner.close();
+        }
     }
 }
