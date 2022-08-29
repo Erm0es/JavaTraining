5add24eb20ec0ad0b733df28e78b66a8aa71a041
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2022-08-29 11:53:07 +0200

    filecrawler test

--- Uppgifter/FileCrawler.java
+++ Uppgifter/FileCrawler.java
@@ -6,29 +6,27 @@ import java.io.*;
 
 public class FileCrawler {
     public static void main (String [] args){
+        try{
         System.out.println("Hi! Enter search word:");
 
         Scanner scanner = new Scanner(System.in);
         String search = scanner.nextLine();
         System.out.println(search);
-        scanner.close();
-
-        readFile("C:\\Users\\Emelie\\Desktop\\Skolan\\FileCrawler");
-    }
-
-    private static void readFile(String filePath){
-        File file = new File(filePath);
-
-        Scanner fileScanner = null;
-        try {
-            fileScanner = new Scanner(file);
-            while(fileScanner.hasNext()) {
-                System.out.println(fileScanner.nextLine());
-            }
-        } catch(FileNotFoundException e){
-            System.out.println("File not found.");
-        } finally {
-            fileScanner.close();
+       
+            BufferedReader br = new BufferedReader(
+                new FileReader("C:\\Users\\Emelie\\Desktop\\Skolan\\FileCrawler\\Stupid.txt"));
+                String s;
+                while((s = br.readLine()) != null){
+                    System.out.println(s);
+                }
+                br.close();
+        }catch(Exception ex){
+            return;
         }
-    }
+
+        if(SÖKORD == br){
+            System.out.Println(s)
+        }
+    }  
+
 }
