65cca60913751375b8541df239a359733787fa98
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2022-08-29 11:34:09 +0200

    Encapsulation

---
+++ Träning/Encapsulation/Main.java
@@ -0,0 +1,12 @@
+package Träning.Encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+     Student s = new Student();
+     s.setName("Suzie");
+     s.setAge(23);
+
+     System.out.println(s.getName());
+     System.out.println(s.getAge());
+    }
+}
