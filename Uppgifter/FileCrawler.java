7a89f9efeb0f030c1e116323d04644d7eba41c7e
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2022-08-30 09:42:13 +0200

    ditched fileReader

--- Uppgifter/FileCrawler.java
+++ Uppgifter/FileCrawler.java
@@ -2,45 +2,44 @@ package Uppgifter;
 
 import java.util.Scanner;
 import java.io.*;
-import java.io.FileReader;
 
 public class FileCrawler {
     public static void main (String [] args){
-
-    readFile("S:\\VisualStudio\\JavaTraining\\Uppgifter\\DocumentFc\\stupid.txt");
-
-    }  
-
-    public static void readFile(String filePath){
         Scanner scanner = null;
-
-        System.out.println("Hi! Enter search word:");
-        scanner = new Scanner(System.in);
-        String search = scanner.nextLine();
+        Scanner fileScanner = null;
 
         try{
-            BufferedReader br = new BufferedReader(
-                new FileReader(filePath));
-                String s;
-
-                while((s = br.readLine()) != null){
-                    if(search.contains(s)){
-                        System.out.println(filePath);
-                    }else{
-                        System.out.println("STUPIDSTUPID");
-                    }
-                    
-                } 
-                br.close();
-                  
-            }catch(Exception e){
-                System.out.println("File does not exist.");
-            }finally{
-                if(scanner != null){
-                    scanner.close();
+            System.out.println("Hi! Enter search word:");
+    
+            scanner = new Scanner(System.in);
+            String search = scanner.nextLine();
+    
+            
+            File file = new File("S:\\VisualStudio\\JavaTraining\\Uppgifter\\DocumentFc\\Stupid.txt");
+            fileScanner = new Scanner(file);
+    
+            while(fileScanner.hasNext()){
+                if(fileScanner.nextLine().contains(search)){
+                    System.out.println(file);
+                } else{
+                    return;
                 }
             }
+           
+        }catch(FileNotFoundException e){
+                System.out.println("File does not exist.");
+        }finally{
+            if(scanner != null){
+                scanner.close();
+            }
+            if(fileScanner != null){
+                fileScanner.close();
+            }
+          
+           
+        }
+         
 
-    }
+    }  
 
 }
