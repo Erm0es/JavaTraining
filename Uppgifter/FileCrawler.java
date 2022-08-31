16865c230affba6cdb3e1f9e31c703a01cd23e13
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2022-08-31 12:00:52 +0200

    cont. work

--- Uppgifter/FileCrawler.java
+++ Uppgifter/FileCrawler.java
@@ -1,45 +1,55 @@
 package Uppgifter;
 
 import java.util.Scanner;
-import java.io.*;
+import java.io.File;
 
 public class FileCrawler {
-    public static void main (String [] args){
+    public static void main(String[] args) {
+        try {
+            File fileBase = new File("S:\\VisualStudio\\JavaTraining\\Uppgifter\\DocumentFc\\stupid.txt");
+            printInfo(fileBase);
+
+        } catch (Exception e) {
+            System.out.println("oops");
+        }
+    }
+
+    public static void printInfo(File file) {
         Scanner scanner = null;
         Scanner fileScanner = null;
-
-        try{
+        
+        try {
             System.out.println("Hi! Enter search word:");
-    
             scanner = new Scanner(System.in);
             String search = scanner.nextLine();
-    
-            
-            File file = new File("S:\\VisualStudio\\JavaTraining\\Uppgifter\\DocumentFc\\Stupid.txt");
+
             fileScanner = new Scanner(file);
-    
-            while(fileScanner.hasNext()){
-                if(fileScanner.nextLine().contains(search)){
-                    System.out.println(file);
-                } else{
-                    return;
+            if (file.isFile() && fileScanner.nextLine().contains(search)) {
+                while (fileScanner.hasNextLine()) {
+                    System.out.println("Word found in File: " + file.getName());
+                    break;
+                }
+
+            } else if (file.isDirectory()) {
+                System.out.println("Word not found. Try this Folder: " + file.getCanonicalPath());
+
+                File[] folderContents = file.listFiles();
+                for (int i = 0; i < folderContents.length; i++) {
+                    File f = folderContents[i];
+                    printInfo(f);
                 }
             }
-           
-        }catch(FileNotFoundException e){
-                System.out.println("File does not exist.");
-        }finally{
-            if(scanner != null){
+
+        } catch (Exception e) {
+            System.err.println("oops. Something went wrong.");
+        } finally {
+            if (scanner != null) {
                 scanner.close();
             }
-            if(fileScanner != null){
+            if (fileScanner != null) {
                 fileScanner.close();
             }
-          
-           
         }
-         
-
-    }  
+    }
 
 }
